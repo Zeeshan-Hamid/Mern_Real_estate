4aa310fea2b7086fb1ab6eebfdde460f567cdee0
Zeeshan Hamid <zeeshanhamid17@gmail.com>
2024-08-02 01:47:26 +0500

    port changed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+//Vite config.js
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
